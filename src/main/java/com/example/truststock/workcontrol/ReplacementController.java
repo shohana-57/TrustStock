b970261811ba391073ae603686dddd8e009e2b91
Shohana Akter Rabina <shohana592059@gmail.com>
2025-12-16 23:40:03 +0600

    replacement page

--- src/main/java/com/example/truststock/workcontrol/ReplacementController.java
+++ src/main/java/com/example/truststock/workcontrol/ReplacementController.java
@@ -1,4 +1,20 @@
 package com.example.truststock.workcontrol;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ReplacementController {
+    public TableView tableProducts;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colStock;
+    public TableColumn colQuality;
+    public TextField txtQuantity;
+    public Button btnReplace;
+
+    public void replaceProduct(ActionEvent actionEvent) {
+    }
 }
